d69e5e4ae38e83d2833832e8d35ca4dfbdd47aef
Iraz Irfan <irazirfan@gmail.com>
2021-01-08 00:58:47 +0600

    adding ChatApp.Domain project

---
+++ ChatApp.Domain/Entity/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatApp.Domain.Entity
+{
+    public class User
+    {
+        [Key]
+        public int UserID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        [EmailAddress]
+        public string Email { get; set; }
+        public string ProfilePicture { get; set; }
+        public string Gender { get; set; }
+        public DateTime DOB { get; set; }
+        public string Bio { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime UpdatedOn { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
